ae8bdb660fdaf38fc5462784bd78bcdfaa96fe23
sourajit <dassour@seas.upenn.edu>
2025-02-05 13:41:08 -0500

    Latest

--- gpt_measurements.py
+++ gpt_measurements.py
@@ -20,9 +20,8 @@ dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
-
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('t8_shakespeare.txt', 'r', encoding='utf-8') as f:
+with open('input.txt', 'r', encoding='utf-8') as f:
+# with open('t8_shakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
