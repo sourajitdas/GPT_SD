ff13156fabefcf6550d28c020d14e4047441aa45
sourajit <dassour@seas.upenn.edu>
2025-02-05 11:26:22 -0500

    Latest

--- gpt_measurements.py
+++ gpt_measurements.py
@@ -10,7 +10,7 @@ batch_size = 64 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
 max_iters = 10000
 eval_interval = 500
-learning_rate = 1e-4
+learning_rate = 2e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 384
@@ -22,7 +22,7 @@ dropout = 0.2
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open('t8_shakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
